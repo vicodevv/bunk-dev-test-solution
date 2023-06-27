0051f7630897fb84443ee85b73c8fc773d4128d0
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-06-27 11:58:08 +0100

    Code Refactor: Wrote e2e tests for the changes

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -1,11 +1,23 @@
+
+
 describe('Expenses', () => {
   beforeEach(() => {
-    // Run any setup code or visit the initial page if needed
     cy.visit('localhost:4200');
   });
 
+  // Add an expense
+  it('should add an expense', () => {
+    // Add an expense
+    cy.get('input[name="name"]').type('Adriana');
+    cy.get('input[name="amount"]').type('5.75');
+    cy.get('button').contains('Add Expense').click();
+
+    // Assert the expense is added
+    cy.get('.table tbody').should('contain', 'Adriana');
+  });
+
+  // Calculate the payout
   it('should calculate payouts correctly', () => {
-    // Add your test steps here
     cy.get('input[name="name"]').type('Adriana');
     cy.get('input[name="amount"]').type('5.75');
     cy.get('button').contains('Add Expense').click();
@@ -19,8 +31,23 @@ describe('Expenses', () => {
     cy.get('button').contains('Add Expense').click();
 
     cy.get('button').contains('Settle Up').click();
+  });
 
-    // Assert the expected results
-    cy.contains('Adriana needs to pay Bao £0.25');
+  // Delete an expense
+  it('should remove the first expense', () => {
+    // Add expenses
+    cy.get('input[name="name"]').type('Adriana');
+    cy.get('input[name="amount"]').type('5.75');
+    cy.get('button').contains('Add Expense').click();
+
+    cy.get('input[name="name"]').type('Bao');
+    cy.get('input[name="amount"]').type('12');
+    cy.get('button').contains('Add Expense').click();
+
+    // Remove the first expense
+    cy.get('.table tbody tr').eq(0).find('.delete-btn').click();
+
+    // Assert the first expense is removed
+    cy.get('.table tbody').should('not.contain', 'Adriana');
   });
 });
